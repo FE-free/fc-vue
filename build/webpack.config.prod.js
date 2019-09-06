d5c5cc8ea8a3e4e96ba8fdb1080d8a289518b45b
luohong123 <3300536651@qq.com>
2019-09-06 10:23:19 +0800

    build production

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -2,7 +2,7 @@
  * @Author: luohong
  * @Date: 2019-08-28 15:55:37
  * @LastEditors: luohong
- * @LastEditTime: 2019-09-06 09:50:29
+ * @LastEditTime: 2019-09-06 10:05:03
  * @Description: 
  * @email: 3300536651@qq.com
  */
@@ -140,6 +140,7 @@ const config = {
         // 打包前先清空
         // vue 加载器，将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块
         new VueLoaderPlugin(),
+        // 解决gh-pages 404问题
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
